4ae674e1af0ce5b5eb31b188edfd69bf96c7f357
87233 <8723323@gmail.com>
2025-11-30 01:04:12 +0200

    noterPage

---
+++ app/notes/page.tsx
@@ -0,0 +1,20 @@
+import { QueryClient } from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
+import type { FetchNotesResponse } from "@/lib/api";
+
+export default async function NotesPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery<FetchNotesResponse>({
+    queryKey: ["notes", "", "created", 1],
+    queryFn: () =>
+      fetchNotes({
+        search: "",
+        page: 1,
+        sortBy: "created",
+      }),
+  });
+
+  return <NotesClient />;
+}
